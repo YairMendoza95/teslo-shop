0d033ced34968672a737a3bd8d3610a015930d5f
YairMendoza95 <joseyair.95@gmail.com>
2023-12-13 05:32:13 -0600

    Autorization v1

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserDto, LoginUserDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
+	private readonly logger = new Logger(AuthController.name);
 	constructor(private readonly authService: AuthService) { }
 
 	@Post('signup')
-	create(@Body() createUserDto: CreateUserDto) {
+	createUser(@Body() createUserDto: CreateUserDto) {
 		return this.authService.create(createUserDto);
+	}
+
+	@Post('login')
+	loginUser(@Body() loginUserDto: LoginUserDto) {
+		return this.authService.login(loginUserDto);
 	}
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,9 @@
-import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User } from './entities/user.entity';
 import * as bcrypt from 'bcrypt';
+import { User } from './entities/user.entity';
+import { CreateUserDto, LoginUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -30,7 +30,20 @@ export class AuthService {
 		}
 	}
 
-	private handleDBException(error: any) {
+	async login(loginUserDto: LoginUserDto) {
+		const { email, password } = loginUserDto;
+		const user = await this.userRepository.findOne({
+			where: { email },
+			select: { email: true, password: true }
+		});
+		if (!user) throw new UnauthorizedException('Credentials are not valid (email)');
+
+		if (!bcrypt.compareSync(password, user.password)) throw new UnauthorizedException('Credentials are not valid (password)');
+
+		return user;
+	}
+
+	private handleDBException(error: any): never {
 		if (error.code === '23505')
 			throw new BadRequestException(error.detail);
 
